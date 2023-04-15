1e665d57db1b026403b0142a87672ae614c433b4
Ariful Hasan <arifhasancu@gmail.com>
2023-04-15 12:11:35 +0600

    Committing this project second time.

--- app/src/main/java/com/example/phoneverification/MainActivity.java
+++ app/src/main/java/com/example/phoneverification/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.phoneverification;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        //mWebView.loadUrl("file:///android_asset/my_animation.html");
+        mWebView.loadUrl("file:///android_asset/animate.html");
+
     }
 }
